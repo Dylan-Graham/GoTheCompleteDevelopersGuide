6586d69305b4bae7ed8c68845dde712750d8a1aa
Dylan Graham <dylan@explore-ai.net>
2022-11-09 13:00:09 +0200

    Clean up

--- Section 1/Cards/deck.go
+++ Section 1/Cards/deck.go
@@ -10,7 +10,7 @@ func newDeck() deck {
 	cardSuits := []string{"Hearts", "Diamonds", "Spades", "Clubs"}
 	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
 
-	// underscores are for variables that we do not care about
+	// underscores are for variables that we do not care about (rids errors of unused variables)
 	for _, cardSuit := range cardSuits {
 		for _, cardValue := range cardValues {
 			cards = append(cards, cardValue + " of " + cardSuit)
@@ -21,7 +21,7 @@ func newDeck() deck {
 }
 
 func (cards deck) print () {
-	for i, card := range cards {
-		fmt.Println(i, card)
+	for _, card := range cards {
+		fmt.Println(card)
 	}
 }
